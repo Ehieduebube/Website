1bb5b2834a1df24e0e7b34f27661ea3a751fe76b
Ebube <ehieduebube@gmail.com>
2024-10-08 02:29:10 -0700

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,191 @@
+const logotext = "Ebube";
+const meta = {
+    title: "Ebube",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Ebube Ehiedu John",
+    animated: {
+        second: "I develop Web Apps ",
+        third: "I develop Websites",
+    },
+    description: "Thank you for visiting my website. I am thrilled at the prospect of working with you to bring your digital dreams to life. Let's embark on a collaborative journey and create something truly extraordinary together.",
+    your_img_url: "/images/me.png",
+};
+
+const dataabout = {
+    title: "about myself",
+    aboutme: "Welcome to my world of web design and development! I'm Ebube, a passionate and experienced professional in creating visually captivating and highly functional websites. I have a strong passion for everything digital, and I want to devote my professional life to turning concepts into breathtaking online experiences. My knowledge of HTML, CSS, JavaScript, and other cutting-edge technologies has improved. In order to create websites that not only meet but also surpass clients' expectations, I make it a point to keep current with the newest trends and best practices. ",
+};
+
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Html & Css",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 65,
+    },
+    {
+        name: "Typescript",
+        value: 65,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Python",
+        value: 40,
+    },
+    {
+        name: "Wordpress",
+        value: 55,
+    },
+    {
+        name: "Php",
+        value: 65,
+    },
+    {
+        name: "C++",
+        value: 45,
+    },
+];
+
+const services = [{
+        title: "JavaScript/Typescript Development",
+        description: "Leveraging the power of JavaScript, i build dynamic and interactive websites/webapps that engage users and provide advanced functionalities. From simple scripts to complex applications, my expertise ensures high-quality code.",
+    },
+    {
+        title: "Responsive Web Design",
+        description: "Ensuring your website looks and works great on all devices is my priority. I create responsive designs that adapt seamlessly to different screen sizes, providing an optimal viewing experience whether on a desktop, tablet, or smartphone.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "My WordPress design services include custom themes and plugins tailored to meet your unique needs. I ensure your website is not only visually appealing but also functional, responsive, and optimized for search engines.",
+    },
+];
+
+const facts =  [{
+    title1: "Passionate Developer",
+    description1: "I am a passionate frontend developer with a love for creating visually appealing and user-friendly websites.",
+},
+{
+    title1: "Tech Enthusiast",
+    description1: "As a tech enthusiast, I stay up-to-date with the latest trends and advancements in frontend development to deliver cutting-edge solutions.",
+},
+{
+    title1: "Problem Solver",
+    description1: "With a knack for problem-solving, I enjoy tackling challenges and finding creative solutions to optimize website performance and functionality.",
+},
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Weather App.",
+        link: "https://vercel.live/link/weather-app-peach-three-88.vercel.app/?via=team-dashboard-project-entity&p=1",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Placeholer.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Placeholder.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Placeholder.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Music Player.",
+        link: "https://vercel.live/link/music-player-rho-wine.vercel.app/?via=team-dashboard-project-entity&p=1",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Placeholder.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Placeholder.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Placeholder.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Crud App.",
+        link: "https://vercel.live/link/crud-blue.vercel.app/?via=team-dashboard-project-entity&p=1",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "Placeholder.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Placeholder.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Placeholder.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ehieduebube@gmail.com",
+    YOUR_FONE: "+234 9068326146",
+    description: "Lagos, Nigeria ",
+    YOUR_SERVICE_ID: "ehieduebube@gmail.com",
+    YOUR_TEMPLATE_ID: "ebube",
+    YOUR_USER_ID: "lssnoR6hqrg1PS4fZ",
+};
+
+const socialprofils = {
+    github: "https://github.com/Ehieduebube",
+    linkedin: "https://www.linkedin.com/in/ebube-ehiedu-88b3b2235",
+    twitter: "https://www.twitter.com/_admiralty/",
+    instagram: "https://www.instagram.com/ad.miralty/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    facts,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
